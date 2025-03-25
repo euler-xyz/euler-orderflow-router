5eda9f7265041520596a67073309320015d42f75
dglowinski <dariushg32111@gmail.com>
2025-03-25 20:39:18 +0100

    allow larger log payloads

--- src/server.ts
+++ src/server.ts
@@ -18,8 +18,8 @@ const app: Express = express()
 // app.set("trust proxy", true);
 
 // Middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: "5mb" }))
+app.use(express.urlencoded({ extended: true, limit: "5mb" }))
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
 app.use(helmet())
 app.use(rateLimiter)
