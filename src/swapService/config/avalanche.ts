c2e8ff0be821da0f026f98fde41ed7bb37e5fc95
dglowinski <dariushg32111@gmail.com>
2025-05-12 08:08:21 +0200

    update config

--- src/swapService/config/avalanche.ts
+++ src/swapService/config/avalanche.ts
@@ -1,12 +1,11 @@
 import { type ChainRoutingConfig, SwapperMode } from "../interface"
 import {
   StrategyBalmySDK,
-  StrategyERC4626Wrapper,
   StrategyElixir,
   StrategyRepayWrapper,
 } from "../strategies"
 
-const SAVUSD_AVALANCHE = "0x06d47F3fb376649c3A9Dafe069B3D6E35572219E"
+// const SAVUSD_AVALANCHE = "a0x06d47F3fb376649c3A9Dafe069B3D6E35572219E"
 const SDEUSD_AVALANCHE = "0x68088C91446c7bEa49ea7Dbd3B96Ce62B272DC96"
 
 const avalancheRoutingConfig: ChainRoutingConfig = [
@@ -16,13 +15,6 @@ const avalancheRoutingConfig: ChainRoutingConfig = [
     match: {
       isRepay: true,
       swapperModes: [SwapperMode.EXACT_IN],
-    },
-  },
-  // SPECIAL CASE
-  {
-    strategy: StrategyERC4626Wrapper.name(),
-    match: {
-      tokensInOrOut: [SAVUSD_AVALANCHE],
     },
   },
   {
