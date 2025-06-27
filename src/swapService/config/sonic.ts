3635770280d83522fa9a558f4efb041eb4fd0723
dglowinski <dariushg32111@gmail.com>
2025-06-27 17:31:42 +0200

    update config

--- src/swapService/config/sonic.ts
+++ src/swapService/config/sonic.ts
@@ -8,7 +8,7 @@ const WSTKSCUSD_SONIC = "0x9fb76f7ce5FCeAA2C42887ff441D46095E494206"
 const WSTKSCETH_SONIC = "0xE8a41c62BB4d5863C6eadC96792cFE90A1f37C47"
 const WOS_SONIC = "0x9F0dF7799f6FDAd409300080cfF680f5A23df4b1"
 const YUSD_SONIC = "0x4772D2e014F9fC3a820C444e3313968e9a5C8121"
-
+const SCUSD_SONIC = "0xd3DCe716f3eF535C5Ff8d041c1A41C3bd89b97aE"
 const sonicConfig: ChainRoutingConfig = [
   // WRAPPERS
   {
@@ -42,7 +42,6 @@ const sonicConfig: ChainRoutingConfig = [
       tokensIn: [YUSD_SONIC],
     },
   },
-  // DEFAULTS
   {
     strategy: StrategyBalmySDK.name(),
     config: {
@@ -59,6 +58,31 @@ const sonicConfig: ChainRoutingConfig = [
           "magpie",
           "pendle",
           // "enso",
+          "okx-dex",
+        ],
+      },
+    },
+    match: {
+      tokensInOrOut: [SCUSD_SONIC],
+    },
+  },
+  // DEFAULTS
+  {
+    strategy: StrategyBalmySDK.name(),
+    config: {
+      sourcesFilter: {
+        includeSources: [
+          "kyberswap",
+          "paraswap",
+          "odos",
+          "1inch",
+          "li-fi",
+          "open-ocean",
+          "uniswap",
+          "0x",
+          "magpie",
+          "pendle",
+          "enso",
           "okx-dex",
         ],
       },
